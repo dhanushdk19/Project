c00b1ee271e2319daaf06bb2a5e8a2d0c63c6206
dhanushdk19 <dhanushksdm@gmail.com>
2024-08-08 09:24:41 -0800

    EMS1

---
+++ Angular/src_for_emp/app/employee.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Employee } from '../Employee';
+import { HttpClient } from '@angular/common/http';
+import { subscribe } from 'node:diagnostics_channel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  url : string;
+  employee : Employee;
+  employeeArr:Employee[];
+
+  constructor(private http:HttpClient) {
+    this.url = "http://localhost:3004/employees";
+    this.employee = new Employee();
+    this.employeeArr=[];
+    
+   }
+
+  insertEmployee(employee : Employee){
+    this.http.post<Employee>(this.url,employee).subscribe();
+    return"Employee Details Added";
+  }
+
+  updateEmployee(employee : Employee){
+    this.http.put<Employee>(this.url+"/"+employee.id,employee).subscribe();
+    return"Employee Details Updated";
+  }
+
+  deleteEmployee(empId : number){
+    this.http.delete<Employee>(this.url+"/"+empId).subscribe();
+    return"Employee Details Deleted";
+  }
+
+  findEmployee(empId : number){
+    this.http.get<Employee>(this.url+"/"+empId).subscribe(data => this.employee = data);
+    return this.employee;
+  }
+
+  findAll(){
+    this.http.get<Employee[]>(this.url).subscribe( data => this.employeeArr = data);
+    return this.employeeArr;
+  }
+}
